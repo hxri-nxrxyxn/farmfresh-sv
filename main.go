8c2a069c714f698587a33c675a1807507973cae3
fahimshafeek <fahimshafeek@gmail.com>
2024-12-15 04:13:07 +0530

    updated the server

--- main.go
+++ main.go
@@ -345,5 +345,10 @@ func main() {
 	http.HandleFunc("/v1/orders", func(w http.ResponseWriter, r *http.Request) {
 		postorders(db, w, r)
 	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	})
 	http.ListenAndServe(":8080", nil)
 }
